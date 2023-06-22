0749c609c5167a364cc6fe9327bfb67ad8b3f271
Tulyakai <tulyawatt@gmail.com>
2023-06-23 00:50:57 +0700

    for nong noob poomkingzaa

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Vue 3 Carousel</h1>
+    <h4>Tulyakai's example for Gamity eiei</h4>
+    <Carousel :games="games" />
+    <Carousel :games="games" />
+    <Carousel :games="games" />
+    <Carousel :games="games" />
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Carousel from './components/CarouselComponent.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Carousel,
+  },
+  data(){
+    return{
+      games: [
+        {
+          name: "Game 1",
+          img: "https://picsum.photos/seed/1/300/500"
+        },
+        {
+          name: "Game 2",
+          img: "https://picsum.photos/seed/2/300/500"
+        },
+        {
+          name: "Game 3",
+          img: "https://picsum.photos/seed/3/300/500"
+        },
+        {
+          name: "Game 4",
+          img: "https://picsum.photos/seed/4/300/500"
+        },
+        {
+          name: "Game 5",
+          img: "https://picsum.photos/seed/5/300/500"
+        },
+        {
+          name: "Game 6",
+          img: "https://picsum.photos/seed/6/300/500"
+        },
+        {
+          name: "Game 7",
+          img: "https://picsum.photos/seed/7/300/500"
+        },
+        {
+          name: "Game 8",
+          img: "https://picsum.photos/seed/8/300/500"
+        },
+        {
+          name: "Game 9",
+          img: "https://picsum.photos/seed/9/300/500"
+        },
+        {
+          name: "Game 10",
+          img: "https://picsum.photos/seed/10/300/500"
+        },
+        {
+          name: "Game 11",
+          img: "https://picsum.photos/seed/11/300/500"
+        },
+        {
+          name: "Game 12",
+          img: "https://picsum.photos/seed/12/300/500"
+        },
+        {
+          name: "Game 13",
+          img: "https://picsum.photos/seed/13/300/500"
+        },
+        {
+          name: "Game 14",
+          img: "https://picsum.photos/seed/14/300/500"
+        },
+        {
+          name: "Game 15",
+          img: "https://picsum.photos/seed/15/300/500"
+        }
+    ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <carousel :items-to-show="5.5">
+      <slide v-for="(game, index) in games" :key="index">
+        <img :src="game.img" />
+      </slide>
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+</template>
+<script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  props: {
+    itemsToShow: {
+      type: Number,
+      default: 5.5,
+      required: false,
+    },
+    games: {
+      default: [],
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+.carousel__icon{
+  color: white !important;
+  font-size: 2rem !important;
+}
+.img{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
